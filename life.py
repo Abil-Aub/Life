11eeda74fa6717f5d4fe3432ebb76696fd3d83a5
Abil-Aub <59198773+Abil-Aub@users.noreply.github.com>
2022-04-06 11:06:10 +0200

    Add files via upload

---
+++ life.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from game_of_life import *
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    GameOfLife(25, 25)
+    return render_template('index.html')
+
+@app.route('/live')
+def live():
+    game = GameOfLife()
+    if game.counter > 0:
+        game.form_new_generation()
+    game.counter += 1
+    return render_template('live.html',
+                           width=range(len(game.world)),
+                           height=range(len(game.world[0])),
+                           world=game.world,
+                           old_world=game.old_world,
+                           counter = game.counter,
+                           )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
